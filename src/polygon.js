f7e15d12d04e7f02f20561dafd0c20a637ef8ade
Faris Hasim <farishasimsyauqi@gmail.com>
2022-02-24 15:08:48 +0700

    update input single polygon

--- src/polygon.js
+++ src/polygon.js
@@ -1,37 +1,30 @@
+
+//  initialize variables
 var gl = null
-
-var vertices = [
-    0, 0.5,
-    -0.75, 0.75,
-    -0.5, 0,
-    -0.75, -0.75,
-    0, -0.5,
-    0.75, -0.75,
-    0.5, 0,
-    0.75, 0.75
-]
-
 var shaderProgram = null
 
 function main() {
-    var canvas = document.getElementById("canvas");
-    gl = canvas.getContext('webgl')
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0, 0, 0, 0.9);
+    // initialization
+    init()
+
+    // set background color to black
+    gl.clearColor(0, 0, 0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    
-    shaderProgram = createShader()
 
-    // var vertex_buffer = gl.createBuffer();
-    // gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    var vertices = []
 
-    // var index_buffer = gl.createBuffer();
-    // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-    // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
-    
-    // gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0)
-    drawPolygon(vertices)
+    document.getElementById("canvas").addEventListener('mousedown', event => {
+        var x = 2*event.clientX/canvas.width-1
+        var y = 2*(canvas.height-event.clientY)/canvas.height-1
+        vertices.push(x, y);
+        console.log(vertices);
+    })
+
+    document.getElementById("draw").addEventListener("click", () => {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        drawPolygon(vertices);
+        vertices = [];
+    });
 }
 
 function drawPolygon(vertices) {
@@ -40,7 +33,7 @@ function drawPolygon(vertices) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     var indices = []
-    for(var i=0; i < vertices.length-2; i++) {
+    for(var i=0; i < vertices.length/2-2; i++) {
         indices.push(0);
         indices.push(i+1);
         indices.push(i+2);
@@ -53,6 +46,7 @@ function drawPolygon(vertices) {
     var coord = gl.getAttribLocation(shaderProgram, "coordinates");
     gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(coord)
+    console.log(indices)
     gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0)
 }
 
